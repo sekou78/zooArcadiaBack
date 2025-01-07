e2a5eff27cc325d44e6e265e955f5f336be5e4a1
sekou78 <s.cisse78@gmail.com>
2025-01-07 16:50:39 +0100

    Création de l'env. local + entity Habitat

---
+++ src/Entity/Habitat.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\HabitatRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: HabitatRepository::class)]
+class Habitat
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $commentHabitat = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCommentHabitat(): ?string
+    {
+        return $this->commentHabitat;
+    }
+
+    public function setCommentHabitat(string $commentHabitat): static
+    {
+        $this->commentHabitat = $commentHabitat;
+
+        return $this;
+    }
+}
