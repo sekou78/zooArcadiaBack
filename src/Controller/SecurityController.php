65583208224781b50d02b6f11217bb1cc191cfc6
sekou78 <s.cisse78@gmail.com>
2025-02-11 16:37:10 +0100

    Documentation de SecurityControlleur

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,8 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Avis;
-use App\Entity\Role;
 use App\Entity\User;
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
@@ -18,6 +16,7 @@ use Symfony\Component\Security\Http\Attribute\{CurrentUser, IsGranted};
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use OpenApi\Attributes as OA;
 
 #[Route('/api', name: 'app_api_')]
 class SecurityController extends AbstractController
@@ -28,6 +27,106 @@ class SecurityController extends AbstractController
         private UserPasswordHasherInterface $passwordHasher
     ) {}
 
+    #[Route('/registration', name: 'registration', methods: 'POST')]
+    #[OA\Post(
+        path: "/api/registration",
+        summary: "Inscription d'un Administrateur",
+        requestBody: new OA\RequestBody(
+            required: true,
+            description: "Admin à inscrire",
+            content: new OA\MediaType(
+                mediaType: "application/json",
+                schema: new OA\Schema(
+                    type: "object",
+                    required: ["email", "password", "roles"],
+                    properties: [
+                        new OA\Property(
+                            property: "email",
+                            type: "string",
+                            format: "email",
+                            example: "adresse@email.com"
+                        ),
+                        new OA\Property(
+                            property: "password",
+                            type: "string",
+                            format: "password",
+                            example: "Azerty$1"
+                        ),
+                        new OA\Property(
+                            property: "roles",
+                            type: "array",
+                            items: new OA\Items(
+                                type: "string",
+                                example: "ROLE_ADMIN"
+                            )
+                        ),
+                        new OA\Property(
+                            property: "username",
+                            type: "string",
+                            example: "Dinga"
+                        ),
+                        new OA\Property(
+                            property: "nom",
+                            type: "string",
+                            example: "Fath"
+                        ),
+                        new OA\Property(
+                            property: "prenom",
+                            type: "string",
+                            example: "Alpha"
+                        )
+                    ]
+                )
+            )
+        ),
+        responses: [
+            new OA\Response(
+                response: 201,
+                description: 'Administrateur inscrit avec succès',
+                content: new OA\MediaType(
+                    mediaType: "application/json",
+                    schema: new OA\Schema(
+                        type: "object",
+                        properties: [
+                            new OA\Property(
+                                property: "user",
+                                type: "string",
+                                example: "Mail de connexions"
+                            ),
+                            new OA\Property(
+                                property: "apiToken",
+                                type: "string",
+                                example: "31a023e212f116124a36af14ea0c1c3806eb9378"
+                            ),
+                            new OA\Property(
+                                property: "roles",
+                                type: "array",
+                                items: new OA\Items(
+                                    type: "string",
+                                    example: "ROLE_ADMIN"
+                                )
+                            ),
+                            new OA\Property(
+                                property: "username",
+                                type: "string",
+                                example: "Dinga"
+                            ),
+                            new OA\Property(
+                                property: "nom",
+                                type: "string",
+                                example: "Fath"
+                            ),
+                            new OA\Property(
+                                property: "prenom",
+                                type: "string",
+                                example: "Alpha"
+                            )
+                        ]
+                    )
+                )
+            )
+        ]
+    )]
     #[Route('/registration', name: 'registration', methods: 'POST')]
     public function register(
         Request $request,
@@ -68,7 +167,7 @@ class SecurityController extends AbstractController
         );
         if ($existingUser) {
             return new JsonResponse(
-                ['error' => 'Email is already in use'],
+                ['error' => 'Email déjà utilisé'],
                 Response::HTTP_BAD_REQUEST
             );
         }
@@ -78,7 +177,7 @@ class SecurityController extends AbstractController
             $existingAdmin = $this->manager->getRepository(User::class)->findOneByRole('ROLE_ADMIN');
             if ($existingAdmin) {
                 return new JsonResponse(
-                    ['error' => 'An admin account already exists. Only one admin can be created.'],
+                    ['error' => 'Un compte administrateur existe déjà. Un seul administrateur peut être créé.'],
                     Response::HTTP_FORBIDDEN
                 );
             }
@@ -116,6 +215,107 @@ class SecurityController extends AbstractController
     }
 
     #[Route('/admin/create-user', name: 'admin_create_user', methods: 'POST')]
+    #[OA\Post(
+        path: "/api/admin/create-user",
+        summary: "Inscription d'un Utilisateur par Administrateur",
+        requestBody: new OA\RequestBody(
+            required: true,
+            description: "Utilisateur à inscrire par Administrateur",
+            content: new OA\MediaType(
+                mediaType: "application/json",
+                schema: new OA\Schema(
+                    type: "object",
+                    required: ["email", "password", "roles"],
+                    properties: [
+                        new OA\Property(
+                            property: "email",
+                            type: "string",
+                            format: "email",
+                            example: "adresse@email.com"
+                        ),
+                        new OA\Property(
+                            property: "username",
+                            type: "string",
+                            example: "Dinga"
+                        ),
+                        new OA\Property(
+                            property: "nom",
+                            type: "string",
+                            example: "Fath"
+                        ),
+                        new OA\Property(
+                            property: "prenom",
+                            type: "string",
+                            example: "Alpha"
+                        ),
+                        new OA\Property(
+                            property: "roles",
+                            type: "array",
+                            items: new OA\Items(
+                                type: "string",
+                                example: ["ROLE_VETERINAIRE"]
+                            ),
+                            example: ["ROLE_EMPLOYE"]
+                        ),
+                        new OA\Property(
+                            property: "password",
+                            type: "string",
+                            format: "password",
+                            example: "Azerty$1"
+                        ),
+                    ]
+                )
+            )
+        ),
+        responses: [
+            new OA\Response(
+                response: 201,
+                description: 'Utilisateur inscrit avec succès',
+                content: new OA\MediaType(
+                    mediaType: "application/json",
+                    schema: new OA\Schema(
+                        type: "object",
+                        properties: [
+                            new OA\Property(
+                                property: "user",
+                                type: "string",
+                                example: "Mail de connexions"
+                            ),
+                            new OA\Property(
+                                property: "apiToken",
+                                type: "string",
+                                example: "31a023e212f116124a36af14ea0c1c3806eb9378"
+                            ),
+                            new OA\Property(
+                                property: "roles",
+                                type: "array",
+                                items: new OA\Items(
+                                    type: "string",
+                                    example: ["ROLE_VETERINAIRE"]
+                                ),
+                                example: ["ROLE_EMPLOYE"]
+                            ),
+                            new OA\Property(
+                                property: "username",
+                                type: "string",
+                                example: "Dinga"
+                            ),
+                            new OA\Property(
+                                property: "nom",
+                                type: "string",
+                                example: "Fath"
+                            ),
+                            new OA\Property(
+                                property: "prenom",
+                                type: "string",
+                                example: "Alpha"
+                            )
+                        ]
+                    )
+                )
+            )
+        ]
+    )]
     #[IsGranted('ROLE_ADMIN')]
     public function createUser(
         Request $request,
@@ -204,6 +404,66 @@ class SecurityController extends AbstractController
     }
 
     #[Route('/login', name: 'login', methods: 'POST')]
+    #[OA\Post(
+        path: "/api/login",
+        summary: "Connexion d'un Utilisateur",
+        requestBody: new OA\RequestBody(
+            required: true,
+            description: "Données de l'utilisateur pour se connecter",
+            content: new OA\MediaType(
+                mediaType: "application/json",
+                schema: new OA\Schema(
+                    type: "object",
+                    required: ["username", "password"],
+                    properties: [
+                        new OA\Property(
+                            property: "username",
+                            type: "string",
+                            example: "adresse@email.com"
+                        ),
+                        new OA\Property(
+                            property: "password",
+                            type: "string",
+                            example: "Azerty$1"
+                        )
+                    ]
+                )
+            )
+        ),
+        responses: [
+            new OA\Response(
+                response: 201,
+                description: "Connexion reussie",
+                content: new OA\MediaType(
+                    mediaType: "application/json",
+                    schema: new OA\Schema(
+                        type: "object",
+                        properties: [
+                            new OA\Property(
+                                property: "user",
+                                type: "string",
+                                example: "Mail de connexions"
+                            ),
+                            new OA\Property(
+                                property: "apiToken",
+                                type: "string",
+                                example: "31a023e212f116124a36af14ea0c1c3806eb9378"
+                            ),
+                            new OA\Property(
+                                property: "roles",
+                                type: "array",
+                                items: new OA\Items(
+                                    type: "string",
+                                    example: ["ROLE_VETERINAIRE"]
+                                ),
+                                example: ["ROLE_EMPLOYE"]
+                            )
+                        ]
+                    )
+                )
+            )
+        ]
+    )]
     public function login(
         #[CurrentUser] ?User $user,
         LoggerInterface $logger
@@ -238,6 +498,58 @@ class SecurityController extends AbstractController
     }
 
     #[Route('/account/me', name: 'me', methods: 'GET')]
+    #[OA\Get(
+        path: "/api/account/me",
+        summary: "Récupérer toutes les informations de l'objet User",
+        responses: [
+            new OA\Response(
+                response: 201,
+                description: "Afficher tous les champs de l'utilisateur",
+                content: new OA\MediaType(
+                    mediaType: "application/json",
+                    schema: new OA\Schema(
+                        type: "object",
+                        properties: [
+                            new OA\Property(
+                                property: "user",
+                                type: "string",
+                                example: "Mail de connexions"
+                            ),
+                            new OA\Property(
+                                property: "apiToken",
+                                type: "string",
+                                example: "31a023e212f116124a36af14ea0c1c3806eb9378"
+                            ),
+                            new OA\Property(
+                                property: "roles",
+                                type: "array",
+                                items: new OA\Items(
+                                    type: "string",
+                                    example: ["ROLE_VETERINAIRE"]
+                                ),
+                                example: ["ROLE_EMPLOYE"]
+                            ),
+                            new OA\Property(
+                                property: "username",
+                                type: "string",
+                                example: "Dinga"
+                            ),
+                            new OA\Property(
+                                property: "nom",
+                                type: "string",
+                                example: "Fath"
+                            ),
+                            new OA\Property(
+                                property: "prenom",
+                                type: "string",
+                                example: "Alpha"
+                            )
+                        ]
+                    )
+                )
+            )
+        ]
+    )]
     public function me(): JsonResponse
     {
         $user = $this->getUser();
@@ -266,6 +578,97 @@ class SecurityController extends AbstractController
     }
 
     #[Route('/account/edit', name: 'edit', methods: 'PUT')]
+    #[OA\Put(
+        path: "/api/account/edit",
+        summary: "Modifier son compte utilisateur avec l'un ou tous les champs",
+        requestBody: new OA\RequestBody(
+            required: true,
+            description: "Nouvelles données éventuelles de l'utilisateur à mettre à jour",
+            content: new OA\MediaType(
+                mediaType: "application/json",
+                schema: new OA\Schema(
+                    type: "object",
+                    properties: [
+                        new OA\Property(
+                            property: "email",
+                            type: "string",
+                            format: "email",
+                            example: "adresse@email.com"
+                        ),
+                        new OA\Property(
+                            property: "username",
+                            type: "string",
+                            example: "Dinga"
+                        ),
+                        new OA\Property(
+                            property: "nom",
+                            type: "string",
+                            example: "Fath"
+                        ),
+                        new OA\Property(
+                            property: "prenom",
+                            type: "string",
+                            example: "Alpha"
+                        ),
+                        new OA\Property(
+                            property: "password",
+                            type: "string",
+                            format: "password",
+                            example: "Azerty$1"
+                        )
+                    ]
+                )
+            )
+        ),
+        responses: [
+            new OA\Response(
+                response: 201,
+                description: "Utilisateur modifié avec succès",
+                content: new OA\MediaType(
+                    mediaType: "application/json",
+                    schema: new OA\Schema(
+                        type: "object",
+                        properties: [
+                            new OA\Property(
+                                property: "user",
+                                type: "string",
+                                example: "Mail de connexions"
+                            ),
+                            new OA\Property(
+                                property: "apiToken",
+                                type: "string",
+                                example: "31a023e212f116124a36af14ea0c1c3806eb9378"
+                            ),
+                            new OA\Property(
+                                property: "roles",
+                                type: "array",
+                                items: new OA\Items(
+                                    type: "string",
+                                    example: ["ROLE_VETERINAIRE"]
+                                ),
+                                example: ["ROLE_EMPLOYE"]
+                            ),
+                            new OA\Property(
+                                property: "username",
+                                type: "string",
+                                example: "Dinga"
+                            ),
+                            new OA\Property(
+                                property: "nom",
+                                type: "string",
+                                example: "Fath"
+                            ),
+                            new OA\Property(
+                                property: "prenom",
+                                type: "string",
+                                example: "Alpha"
+                            )
+                        ]
+                    )
+                )
+            )
+        ]
+    )]
     public function edit(
         Request $request,
         CsrfTokenManagerInterface $csrfTokenManager,
